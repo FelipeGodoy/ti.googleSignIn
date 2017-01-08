2a2e4f529cfb49c02fc2c09965065cda7346ad02
Codezone <codezone@CODEZONE-MACMINI.local>
2017-01-08 15:08:40 -0200

    iphone module initial commit

---
+++ iphone/Classes/TiAuthtimesheetModule.h
@@ -0,0 +1,14 @@
+/**
+ * authtimesheet
+ *
+ * Created by Your Name
+ * Copyright (c) 2017 Your Company. All rights reserved.
+ */
+
+#import "TiModule.h"
+
+@interface TiAuthtimesheetModule : TiModule
+{
+}
+
+@end
